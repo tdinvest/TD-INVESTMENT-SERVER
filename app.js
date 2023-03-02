f579e4a9825bafa91499fc0ffa67892cb9705115
richard <richardgraythom@gmail.com>
2023-03-02 08:20:14 -0800

    w

--- app.js
+++ app.js
@@ -104,28 +104,25 @@ app.use(bodyParser.json()); // application/json
 // app.use('/images', express.static(path.join(__dirname, 'images')));
 
 
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: ['GET,OPTIONS,PATCH,DELETE,POST,PUT']
+}));
 
 
 app.use( async(req, res, next) => {
-res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
   // another common pattern
   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
   res.setHeader(
     'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  );
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
+    'Content-Length'
+  )
   next();
 });
 
-app.use(cors({
-  origin: 'http://localhost:3000',
-}));
 
 app.use('/feed', feedRoutes);
 app.use('/auth', authRoutes);
