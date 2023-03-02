7732d41024ba5d2fc30055775d99d3617ce99953
richard <richardgraythom@gmail.com>
2023-03-02 07:31:07 -0800

    corsett

--- app.js
+++ app.js
@@ -94,9 +94,9 @@ const app = express();
 //     console.error(error);
 //   });
 
-app.use(cors({
-  origin: 'http://localhost:3000',
-}));
+// app.use(cors({
+//   origin: 'http://localhost:3000',
+// }));
 
 
 app.use(bodyParser.urlencoded({extended: true})); // x-www-form-urlencoded <form>
@@ -107,12 +107,15 @@ app.use(bodyParser.json()); // application/json
 // app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
+res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader('Access-Control-Allow-Origin', '*');
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
   res.setHeader(
-    'Access-Control-Allow-Methods',
-    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
   );
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
